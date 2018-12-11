749e150f6a4c370f8ef9761f09a7a64607f12804
Sam Dalton <spd4nf@virginia.edu>
2018-12-11 11:06:49 -0600

    finished figuring out Big O on drill functions

--- functions.js
+++ functions.js
@@ -10,6 +10,7 @@ function isEven(value){
     return false;
 }
 
+//O(1)
 
 //Are you here?
 
@@ -24,6 +25,8 @@ function areYouHere(arr1, arr2) {
   return false;
 }
 
+//O(n^2)
+
 //Doubler
 
 function doubleArrayValues(array) {
@@ -32,6 +35,8 @@ function doubleArrayValues(array) {
   }
   return array;
 }
+
+//O(n)
 
 //Naive Search
 
@@ -43,15 +48,30 @@ function naiveSearch(array, item) {
   }
 }
 
+//O(n)
+
 //Creating pairs
 
 function createPairs(arr) {
+  let count =0;
   for (let i = 0; i < arr.length; i++) {
     for (let j = i + 1; j < arr.length; j++) {
       console.log(arr[i] + ', ' + arr[j]);
+      count++;
     }
   }
+  console.log(count);
 }
+
+//createPairs([1,2,3,4,5,6,7,8,9,10]);
+
+//4, 6
+//8, 28
+//10, 45
+//20, 190
+//30, 435
+
+//O(2^n)
 
 //Computing fibonaccis
 
@@ -81,16 +101,21 @@ function generateFib(num) {
   return result;
 }
 
+//O(n)
+
 //An efficient search
 
 function efficientSearch(array, item) {
   let minIndex = 0;
+  let count = 0;
   let maxIndex = array.length - 1;
   let currentIndex;
   let currentElement;
 
   while (minIndex <= maxIndex) {
+    count++;
     currentIndex = Math.floor((minIndex + maxIndex) / 2);
+    console.log('min:' + minIndex,'max:'+maxIndex,'current:'+currentIndex);
     currentElement = array[currentIndex];
 
     if (currentElement < item) {
@@ -100,17 +125,27 @@ function efficientSearch(array, item) {
       maxIndex = currentIndex - 1;
     }
     else {
+      console.log(count);
       return currentIndex;
     }
+    
   }
+  console.log(count);
   return -1;
 }
+
+//const answer = efficientSearch([1,2,3,4,5,6,7,8,9,10],4);
+//console.log(answer);
+
+//O(log(n))
 
 //Random element
 
 function findRandomElement(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
+
+//O(1)
 
 //Is it prime?
 
@@ -121,8 +156,34 @@ function isPrime(n) {
   }
   // otherwise, check if `n` is divisible by any integer
   // between 2 and n.
+  let count=0;
   for (let i = 2; i < n; ++i) {
-    if (n % i === 0) return false;
+    count++;
+    if (n % i === 0) {
+      console.log(count);
+      return false;
+    }
   }
+  console.log(count);
   return true;
 }
+
+console.log(isPrime(1009));
+
+//13, 11, true
+//19, 17, true
+//211, 209, true
+//144, 1, false
+//145, 4, false
+//4009, 18, false
+
+//O(n), worst case scenario the number is prime and you have to do num-2 operations
+
+/*
+
+If num isPrime, # of operations = num-2;
+If num !isPrime, # of operations is dependent on number
+If num%2===0, # of operations is 1
+If num%3===0, # of operations is 2
+
+*/
